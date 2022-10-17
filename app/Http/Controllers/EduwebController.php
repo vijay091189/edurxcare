8e7fe246a922b5eaaffe37cb2b5a6369e316e3fe
vijay091189 <vijju.0911@gmail.com>
2022-10-17 23:30:54 +0530

    changes in controller

--- app/Http/Controllers/EduwebController.php
+++ app/Http/Controllers/EduwebController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class EduwebController extends Controller
 {
     public function homepage(Request $request){
-        return view("welcome")->with($data);
+        return view("welcome");
     }
 }
